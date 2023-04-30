d32a9d63418c9f6d59fc288d41c6cee817f327a4
Linus Ramneborg <uberhaxor101@live.com>
2023-05-01 01:09:57 +0200

    asd

--- bot/startup.lua
+++ bot/startup.lua
@@ -1,5 +1,31 @@
-function mine(width, height)
+local fuelLevel = turtle.getFuelLevel()
+local maxLevel = turtle.getFuelLimit()
+
+function needsFuel(moveDistance) 
+    fuelLevel = turtle.getFuelLevel()
+    if moveDistance ~= nil then 
+        if fuelLevel-moveDistance <= 0 then 
+            return true
+        end
+    else 
+        if fuelLevel-1 <= 0 then 
+            return true 
+        end
+    end
+    return false
+end
 
 
+function mine(xWidth, zWidth, depth)
+    -- Begin by mining block underneath
+    turtle.digDown()
+    turtle.down()
+    for layer=1, depth, 1 do 
+        for fwd=1, zWidth, 1 do 
+            turtle.dig()
+            turtle.forward()
+        end
+    end
 end
 print("minebot")
+mine(3,3,3)
