09592394e03b64b2557d6b202363515c005257e8
Linus Ramneborg <uberhaxor101@live.com>
2023-05-01 01:12:20 +0200

    asdasdasd

--- bot/startup.lua
+++ bot/startup.lua
@@ -18,13 +18,29 @@ end
 
 function mine(xWidth, zWidth, depth)
     -- Begin by mining block underneath
+    local forward = true
     turtle.digDown()
     turtle.down()
     for layer=1, depth, 1 do 
-        for fwd=1, zWidth, 1 do 
-            turtle.dig()
-            turtle.forward()
+        for right=1, xWidth, 1 do 
+            for fwd=1, zWidth, 1 do 
+                turtle.dig()
+                turtle.forward()
+            end
+            if forward then 
+                turtle.turnRight()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnRight()
+            else 
+                turtle.turnLeft()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnLeft()
+            end
+            forward = not forward
         end
+        
     end
 end
 print("minebot")
